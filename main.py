72a3be8814991ec2aa554ad9495e99f1ea0cb3f2
Nuha-hub <134822512+Nuha-hub@users.noreply.github.com>
2024-04-27 10:54:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,62 @@ from time import time
 import os
 import random	
 import time
+from AegosPy import *
+#-------------------------------------------------#
+def oo(email):
+	url='https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/'
+	haha={
+		'X-Pigeon-Session-Id':'2b712457-ffad-4dba-9241-29ea2f472ac5',
+		'X-Pigeon-Rawclienttime':'1707104597.347',
+		'X-IG-Connection-Speed':'-1kbps',
+		'X-IG-Bandwidth-Speed-KBPS':'-1.000',
+		'X-IG-Bandwidth-TotalBytes-B':'0',
+		'X-IG-Bandwidth-TotalTime-MS':'0',
+		'X-IG-VP9-Capable':'false',
+		'X-Bloks-Version-Id':'009f03b18280bb343b0862d663f31ac80c5fb30dfae9e273e43c63f13a9f31c0',
+		'X-IG-Connection-Type':'WIFI',
+		'X-IG-Capabilities':'3brTvw==',
+		'X-IG-App-ID':'567067343352427',
+		'User-Agent':'Instagram 100.0.0.17.129 Android (30/11; 320dpi; 720x1448; realme; RMX3231; RMX3231; RMX3231; ar_IQ; 161478664)',
+		'Accept-Language':'ar-IQ, en-US',
+		'Cookie':'mid=Zbu4xQABAAE0k2Ok6rVxXpTD8PFQ; csrftoken=dG4dEIkWvAWpIj1B2M2mutWtdO1LiPCK',
+		'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+		'Accept-Encoding':'gzip, deflate',
+		'Host':'i.instagram.com',
+		'X-FB-HTTP-Engine':'Liger',
+		'Connection':'keep-alive',
+		'Content-Length':'364',
+		}
+	ada={
+		'signed_body':'ef02f559b04e8d7cbe15fb8cf18e2b48fb686dafd056b7c9298c08f3e2007d43.{"_csrftoken":"dG4dEIkWvAWpIj1B2M2mutWtdO1LiPCK","adid":"5e7df201-a1ff-45ec-8107-31b10944e25c","guid":"b0382b46-1663-43a7-ba90-3949c43fd808","device_id":"android-71a5d65f74b8fcbc","query":"'f'{email}''"}',
+		'ig_sig_key_version':'4'
+		}
+	s=requests.post(url,headers=haha,data=ada).json()
+	try:
+		return s['email']
+	except:
+		return s
+#-------------------------------------------------#
+def info(email,chat_id):
+	if '@' in email:
+		email=email.split('@')[0]
+	rest=oo(email)
+	Response = GetInfoInsta(email)
+	if 'name' in Response:
+	    Name = Response['name']
+	    Id = Response['id']
+	    flos = Response['followers']
+	    flog = Response['following']
+	    po = Response['posts']
+	    da = Response['date']
+	    tlg =  f"ᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓ\n🗣 NAME  :  {Name}\n🎫 UESR :  @{email}\n📧 EMAIL : {email}@gmail.com\n🗞 ID : {Id}\n🎎 FOLLOWERS : {flos}\n🎎 FOLLOWING : {flog}\n🎆 POST >> {po}\n📅 DATE : {da}\n🔭 REST : {rest}\n🟢 LINK : https://www.instagram.com/{email}\nᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓᯓ"
+	    bot.send_message(chat_id,tlg)
+	else:
+	    bot.send_message(chat_id,f"bad info\nemail:{email}\nrest:{rest}")
 #-------------------------------------------------#
 good=0
 bad=0
-bot = telebot.TeleBot('7061973890:AAH3CBJ-SJyj97DcZV9_CTi2n8dTULbJFoE')
+bot = telebot.TeleBot('6548321283:AAE1Mbhxeg4T4NjJ3U7CYOlibKSpc-grv00')
 user_step = {}
 #-------------------------------------------------#
 def hotmail(email):
@@ -133,9 +185,11 @@ def handle_document(message):
 def check_account(email, chat_id):
 	global good , bad
 	oo = chkhh(email)
+	print(oo)
 	if "good" in oo:
 		good+=1
-		bot.send_message(chat_id,f"- {oo}\n- email:{email}")
+		info(email,chat_id)
+		#bot.send_message(chat_id,f"- {oo}\n- email:{email}")
 	else:
 		bad += 1
 #-------------------------------------------------#
